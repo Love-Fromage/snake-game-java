3c2fcfffed78f4ca635023a10413d17eca0dee9b
Love-Fromage <mathecool22@gmail.com>
2023-03-07 13:32:56 -0500

    clean up pour faire mon code

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,5 +4,18 @@ public class SnakeGame {
 
     private static final int BOARD_WIDTH = 20;
     private static final int BOARD_HEIGHT = 20;
-    
+
+    private static final int UP = 1;
+    private static final int DOWN = 2;
+    private static final int LEFT = 3;
+    private static final int RIGHT = 4;
+
+    private static int direction;
+    private static int score;
+    private static boolean gameOver;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my snake game!");
+    }
+
 }
