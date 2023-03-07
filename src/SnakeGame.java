92698e1fc0c7d472d610991ea3aab708c332925e
Love-Fromage <mathecool22@gmail.com>
2023-03-07 14:18:51 -0500

    yoo le S y bougegit add . !

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,33 +4,58 @@ public class SnakeGame {
 
     private static final int BOARD_WIDTH = 15;
     private static final int BOARD_HEIGHT = 15;
-    private static int[][] grid;
-    private static int posX = 20;
-    private static int posY = 12;
-
-    // private static final int UP = 1;
-    // private static final int DOWN = 2;
-    // private static final int LEFT = 3;
-    // private static final int RIGHT = 4;
-
-    private static int direction;
+    private static char[][] grid = new char[BOARD_WIDTH][BOARD_HEIGHT];
+    private static int posX;
+    private static int posY;
     private static int score;
     private static boolean gameOver;
 
     public static void main(String[] args) {
+        // on initialise la grid avec du vide (spaces)
+        for (int i = 0; i < BOARD_WIDTH; i++) {
+            for (int j = 0; j < BOARD_HEIGHT; j++) {
+                grid[j][i] = ' ';
+            }
+        }
         System.out.println("Welcome to my snake game!");
+        posX = 5;
+        posY = 5;
+        Display(posX, posY);
+        int a = 0;
+        while (a < 5) {
 
-        // ici je vais essayer de print out une grid
+            try {
+                posX++;
+                Thread.sleep(1000);
+                grid[posX - 1][posY] = ' ';
+                clearScreen();
+                Display(posX, posY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            a++;
+        }
+
+    }
+
+    private static void Display(int x, int y) {
+        grid[x][y] = 'S';
         for (int i = 0; i < BOARD_WIDTH; i++) {
             for (int j = 0; j < BOARD_HEIGHT; j++) {
                 if (i == 0 || i == BOARD_WIDTH - 1 || j == 0 || j == BOARD_HEIGHT - 1) {
                     System.out.print("# ");
                 } else {
-                    System.out.print("  ");
+                    System.out.print(grid[j][i] + " ");
                 }
             }
             System.out.println();
         }
+
+    }
+
+    private static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
 }
