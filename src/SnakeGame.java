207606854920cb56b77278b9f191b2c6f6e73135
Love-Fromage <mathecool22@gmail.com>
2023-03-07 13:54:09 -0500

    ajout d'une grid a la main

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class SnakeGame {
 
-    private static final int BOARD_WIDTH = 20;
-    private static final int BOARD_HEIGHT = 20;
+    private static final int BOARD_WIDTH = 15;
+    private static final int BOARD_HEIGHT = 15;
+    private static int[][] grid;
+    private static int posX = 20;
+    private static int posY = 12;
 
-    private static final int UP = 1;
-    private static final int DOWN = 2;
-    private static final int LEFT = 3;
-    private static final int RIGHT = 4;
+    // private static final int UP = 1;
+    // private static final int DOWN = 2;
+    // private static final int LEFT = 3;
+    // private static final int RIGHT = 4;
 
     private static int direction;
     private static int score;
@@ -16,6 +19,18 @@ public class SnakeGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my snake game!");
+
+        // ici je vais essayer de print out une grid
+        for (int i = 0; i < BOARD_WIDTH; i++) {
+            for (int j = 0; j < BOARD_HEIGHT; j++) {
+                if (i == 0 || i == BOARD_WIDTH - 1 || j == 0 || j == BOARD_HEIGHT - 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 
 }
