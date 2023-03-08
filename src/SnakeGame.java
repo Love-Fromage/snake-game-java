55a73f835fbe84b4cda21441dd2535d22946196c
Love-Fromage <mathecool22@gmail.com>
2023-03-07 19:36:40 -0500

    affichage d'une grille en graphics

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,4 @@
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyListener;
@@ -7,93 +6,157 @@ import java.awt.event.KeyEvent;
 import java.util.*;
 
 public class SnakeGame extends JFrame implements KeyListener {
-    private static JTextArea console;
+
+    private static final int BOARD_WIDTH = 20;
+    private static final int BOARD_HEIGHT = 20;
+    private static final int SQUARE_SIZE = 20;
+    private static final int PADDING = 5;
+    private static Color[][] grid = new Color[BOARD_WIDTH][BOARD_HEIGHT];
+    private static int posX;
+    private static int posY;
+    private static int score;
+    private static String direction = "right";
+    private static boolean gameOver;
 
     public SnakeGame() {
         this.setSize(700, 500);
+        setLocationRelativeTo(null);
         setTitle("Snake Game!");
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // create a new JTextArea to display console output
-        console = new JTextArea();
-        console.setEditable(false);
-
-        // Add the JTextArea to a JScrollPane and add it to the frame
-        getContentPane().add(console);
         this.getContentPane().setBackground(Color.BLACK);
-        console.setBackground(Color.BLACK);
-        console.setForeground(Color.GREEN);
-        // Add Keylistener // Add KeyListener to JTextArea
-        console.addKeyListener(this);
-        console.setColumns(55);
-        console.setRows(55);
 
         // Make the frame visible
         setVisible(true);
     }
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        System.out.println("Key pressed : " + e.getKeyCode());
-    }
+    public void paint(Graphics g) {
+        super.paint(g);
 
-    private static final int BOARD_WIDTH = 20;
-    private static final int BOARD_HEIGHT = 20;
-    private static char[][] grid = new char[BOARD_WIDTH][BOARD_HEIGHT];
-    private static int posX;
-    private static int posY;
-    private static int score;
-    private static boolean gameOver;
+        // loop through each row and column and draw a rectangle
+        for (int row = 0; row < BOARD_WIDTH; row++) {
+            for (int col = 0; col < BOARD_HEIGHT; col++) {
+                int x = col * (SQUARE_SIZE + PADDING); // x-coordinate of top-left corner
+                int y = row * (SQUARE_SIZE + PADDING); // y-coordinate of top-left corner
+                if (row == 0 || row == BOARD_WIDTH - 1 || col == 0 || col == BOARD_HEIGHT - 1) {
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(Color.BLACK);
+                }
+                // g.setColor(grid[row][col]); // set color from the 2D array
+                g.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SnakeGame snek = new SnakeGame();
-        // on initialise la grid avec du vide (spaces)
-        for (int i = 0; i < BOARD_WIDTH; i++) {
-            for (int j = 0; j < BOARD_HEIGHT; j++) {
-                grid[j][i] = ' ';
-            }
-        }
-        console.append("Welcome to my snake game!\n");
+
+        // // on initialise la grid avec du vide (spaces)
+        // for (int i = 0; i < BOARD_WIDTH; i++) {
+        // for (int j = 0; j < BOARD_HEIGHT; j++) {
+        // grid[j][i] = ' ';
+        // }
+        // }
+        // console.append("Welcome to my snake game!\n");
         posX = 5;
         posY = 5;
-        Display(posX, posY);
+        // Display(posX, posY);
         int a = 0;
-        while (a < 10) {
+        // try {
+        // while (!gameOver) {
+        // Thread.sleep(1000);
+        // if (direction == "right") {
+        // posX++;
+        // grid[posX - 1][posY] = ' ';
+        // clearScreen();
+        // Display(posX, posY);
+        // }
+        // if (direction == "down") {
+        // posY++;
+        // grid[posX][posY - 1] = ' ';
+        // clearScreen();
+        // Thread.sleep(1000);
+        // Display(posX, posY);
+        // }
+        // if (direction == "left") {
+        // posX--;
+        // grid[posX + 1][posY] = ' ';
+        // clearScreen();
+        // Thread.sleep(1000);
+        // Display(posX, posY);
+        // }
+        // if (direction == "up") {
+        // posY--;
+        // grid[posX][posY + 1] = ' ';
+        // clearScreen();
+        // Thread.sleep(1000);
+        // Display(posX, posY);
+        // }
+        // }
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
+        // while (a < 10) {
 
-            try {
-                posX++;
-                Thread.sleep(1000);
-                grid[posX - 1][posY] = ' ';
-                clearScreen();
-                Display(posX, posY);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            a++;
-        }
+        // try {
+        // posX++;
+        // Thread.sleep(1000);
+        // grid[posX - 1][posY] = ' ';
+        // clearScreen();
+        // Display(posX, posY);
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
+        // a++;
+        // }
 
     }
 
-    private static void Display(int x, int y) {
-        grid[x][y] = 'S';
-        for (int i = 0; i < BOARD_WIDTH; i++) {
-            for (int j = 0; j < BOARD_HEIGHT; j++) {
-                if (i == 0 || i == BOARD_WIDTH - 1 || j == 0 || j == BOARD_HEIGHT - 1) {
-                    console.append("# ");
-                } else {
-                    console.append(grid[j][i] + "  ");
-                }
-            }
-            console.append("\n");
-            // console.append("" + console.getColumns());
-        }
+    // private static void Display(int x, int y) {
+    // grid[x][y] = 'S';
+    // for (int i = 0; i < BOARD_WIDTH; i++) {
+    // for (int j = 0; j < BOARD_HEIGHT; j++) {
+    // if (i == 0 || i == BOARD_WIDTH - 1 || j == 0 || j == BOARD_HEIGHT - 1) {
+    // // console.append("# ");
+    // } else {
+    // // console.append(grid[j][i] + " ");
+    // }
+    // }
+    // // console.append("\n");
+    // // console.append("" + console.getColumns());
+    // }
 
-    }
+    // }
 
     private static void clearScreen() {
-        console.setText("");
+        // console.setText("");
 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed : " + e.getKeyCode());
+        // 65 =a
+        // 87 = w
+        // 68 = d
+        // 83 =s
+        if (e.getKeyCode() == 65) {
+            direction = "left";
+            // console.append(direction + "\n");
+        }
+        if (e.getKeyCode() == 87) {
+            direction = "up";
+            // console.append(direction + "\n");
+        }
+        if (e.getKeyCode() == 68) {
+            direction = "right";
+            // console.append(direction + "\n");
+        }
+        if (e.getKeyCode() == 83) {
+            direction = "down";
+            // console.append(direction + "\n");
+        }
     }
 
     @Override
